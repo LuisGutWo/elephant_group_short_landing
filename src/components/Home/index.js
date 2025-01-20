7d7d43c11241b43804be9f5517959d6094e6d164
LuisGutWo <agutierrezwong@gmail.com>
2025-01-20 04:13:49 -0400

    feat: Home main section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,8 +3,14 @@ import Loader from 'react-loaders'
 import LogoTitle from '../../assets/images/elephant_group_logo.png'
 import './index.scss'
 import 'animate.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { BsEnvelopeArrowUp } from 'react-icons/bs'
+import { GiRolledCloth } from 'react-icons/gi'
+import { PiPhoneCallFill } from 'react-icons/pi'
+import { FiMapPin } from 'react-icons/fi'
 
-const Home = () => {
+function Home() {
   return (
     <>
       <div className="container home-page">
@@ -20,21 +26,62 @@ const Home = () => {
             <br />
             con precisión, calidad y rapidez garantizada.
           </h1>
-
-          <Link to="/contact" className="flat-button">
-            CONTÁCTEME
-          </Link>
-          <Link to="/contact" className="flat-button">
-            CONTÁCTEME
-          </Link>
-          <Link to="/contact" className="flat-button">
-            CONTÁCTEME
-          </Link>
-          <Link to="/contact" className="flat-button">
-            CONTÁCTEME
-          </Link>
-          <h2>Desarrollador Front-End</h2>
         </div>
+
+        <article className="buttons-border-zone mt-4 animate__animated animate__fadeIn">
+          <div className="text-zone mt-4 d-flex justify-content-center flex-column align-items-center animate__animated animate__fadeIn">
+            <a
+              rel="noreferrer"
+              href="https://api.whatsapp.com/send?phone=56951631370&text=Hola"
+              target="_blank"
+              className="flat-button"
+            >
+              <div className="ico">
+                <FontAwesomeIcon size="3x" icon={faWhatsapp} />
+              </div>
+              <span className="text animate__animated animate__fadeIn">
+                WhatsApp
+              </span>
+            </a>
+            <Link to="/contact" className="flat-button">
+              <FontAwesomeIcon size="3x" icon={faInstagram} />
+              <span className="text animate__animated animate__fadeIn">
+                Instagram
+              </span>
+            </Link>
+            <Link to="/contact" className="flat-button">
+              <BsEnvelopeArrowUp className="icon" />
+              <span className="text animate__animated animate__fadeIn">
+                Correo
+              </span>
+            </Link>
+            <Link to="/contact" className="flat-button">
+              <GiRolledCloth className="icon" />
+              <span className="text animate__animated animate__fadeIn">
+                Catalogo
+              </span>
+            </Link>
+          </div>
+        </article>
+        <article className="d-flex justify-content-center flex-row align-items-center animate__animated animate__fadeIn">
+          <div className="buttons-border-zone mt-4 d-flex justify-content-center flex-column align-items-center animate__animated animate__fadeIn">
+            <Link to="/contact" className="flat-button">
+              <PiPhoneCallFill className="icon" />
+              <span className="text-2 animate__animated animate__fadeIn">
+                Llámanos
+              </span>
+            </Link>
+          </div>
+          <div className="buttons-border-zone mt-4 d-flex justify-content-center flex-column align-items-center animate__animated animate__fadeIn">
+            <Link to="/contact" className="flat-button">
+              <FiMapPin className="icon" />
+              <span className="text-2 animate__animated animate__fadeIn">
+                Ubicación
+              </span>
+            </Link>
+          </div>
+        </article>
+        <footer>Desarrollador Front-End</footer>
       </div>
 
       <Loader type="line-scale-pulse-out" />
